a17f0cf979ecea7cfa99b01edad34c4bd38b625d
Jordy Coder <59865566+darksos34@users.noreply.github.com>
2026-02-22 08:33:09 +0100

    git init

---
+++ src/main/java/com/jda/coder/cleangithistory/CleanGitHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.jda.coder.cleangithistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanGitHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanGitHistoryApplication.class, args);
+    }
+
+}
